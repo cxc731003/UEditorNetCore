bdde23b7ea495b72ccf648007676aedc59932b5c
int2e <int2e@qq.com>
2017-12-15 22:14:33 +0800

    升级到.net core 2.0

--- src/Sample/Startup.cs
+++ src/Sample/Startup.cs
@@ -4,40 +4,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using UEditorNetCore;
-using Microsoft.AspNetCore.Http;
 
 namespace Sample
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
+            Configuration = configuration;
         }
 
-        public IConfigurationRoot Configuration { get; }
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddUEditorService()
-                .Add("test", context =>
-                {
-                    context.Response.WriteAsync("from test action");
-                })
-                .Add("test2", context =>
-                {
-                    context.Response.WriteAsync("from test2 action");
-                });
+                  .Add("test", context =>
+                  {
+                      context.Response.WriteAsync("from test action");
+                  })
+                  .Add("test2", context =>
+                  {
+                      context.Response.WriteAsync("from test2 action");
+                  });
             services.AddMvc();
         }
 
@@ -46,7 +41,6 @@ namespace Sample
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
